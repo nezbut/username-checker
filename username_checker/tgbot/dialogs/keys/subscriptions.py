06a7109f78c150486098b4cd7ad80247c5343917
NezBut <xyi12312354@mail.ru>
2024-10-31 17:41:38 +0700

    feat(tgbot/dialogs): add keys for  dialogs

---
+++ username_checker/tgbot/dialogs/keys/subscriptions.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class SubscriptionsDialogDataKeys(str, Enum):
+
+    """Enum class for subscriptions dialog data keys."""
+
+    CURRENT_SUBSCRIPTION_ID = "current_sub_id"
